8bff8c1524951a0f27647be5dbf3f0b1837a2e00
Samuel Šulovský <samuel.sulovsky@gmail.com>
2024-12-01 13:28:05 +0800

    Solve day 1 📝

--- src/day1.ts
+++ src/day1.ts
@@ -1,4 +1,56 @@
-import { getInputFromFile } from "./utils.js"
-console.log('Hello AoC :)')
-const a = await getInputFromFile(1, true)
-console.log(a)
+import { A } from '@mobily/ts-belt'
+import { getInput } from "./utils.js"
+
+const part1 = async () => {
+        const lines = await getInput(1, false)
+        const pairs = lines.map((line) =>
+                line.split(' ')
+                        .map(it => it.trim())
+                        .filter(it => it != '')
+                        .map(it => Number.parseInt(it))
+        )
+        const lists: [number[], number[]] = pairs.reduce<[number[], number[]]>((acc, curr) => {
+                return [[...acc[0], curr[0]], [...acc[1], curr[1]]]
+        }, [[], []])
+
+        lists[0] = lists[0].sort()
+        lists[1] = lists[1].sort()
+
+        let count = 0
+        for (let i = 0; i < lists[0].length; i++) {
+                count += Math.abs(lists[0][i] - lists[1][i])
+        }
+        return count
+}
+
+const part2 = async () => {
+        const lines = await getInput(1, false)
+        const pairs = lines.map((line) =>
+                line.split(' ')
+                        .map(it => it.trim())
+                        .filter(it => it != '')
+                        .map(it => Number.parseInt(it))
+        )
+        const lists: [number[], number[]] = pairs.reduce<[number[], number[]]>((acc, curr) => {
+                return [[...acc[0], curr[0]], [...acc[1], curr[1]]]
+        }, [[], []])
+
+        lists[0] = lists[0].sort()
+        lists[1] = lists[1].sort()
+
+        // I still can't believe js doesn't have Array.uniq
+        const counts = A.uniq(lists[0])
+                .map((it) => ({ [it]: 0 }))
+                .reduce((acc, curr) => ({ ...acc, ...curr }))
+        const tests = lists[1]
+        tests.forEach((n) => {
+                if (typeof counts[n] === 'number') {
+                        counts[n] += 1
+                }
+        })
+        return lists[0].map((n) => n * (counts[n] ? counts[n] : 0))
+                .reduce((acc, curr) => acc + curr, 0)
+}
+
+console.log('Part 1: ', await part1())
+console.log('Part 2: ', await part2())
